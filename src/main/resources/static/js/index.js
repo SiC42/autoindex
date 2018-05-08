537b9d046ef2e64ac0d32ee048a08d0b00bfa003
Prashanth <prasanthhs@yahoo.com>
2018-05-08 22:52:40 +0200

    UI changes to support text area inputs
    1. Store user changes in a variable. Also collapse the widgets on press of Save/Cancel.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,31 @@
+var entity_input = "";
+var property_input = "";
+var class_input = "";
+var changeEntity = function(){
+	entity_input = document.getElementById("entity_textarea").value;
+	document.getElementById("collapsible_entity").click(); 
+	//TODO: Don't think this is a good fix. Need to probably find better solution,ask.
+};
+var changeProperty = function(){
+	property_input = document.getElementById("property_textarea").value;
+	document.getElementById("collapsible_property").click(); 
+};
+var changeClass = function(){
+	class_input = document.getElementById("class_textarea").value;
+	document.getElementById("collapsible_class").click(); 
+};
+var handleEntityCancel = function(){
+	document.getElementById("collapsible_entity").click(); 
+	document.getElementById("entity_textarea").value = entity_input; //Putback the old value
+};
+var handlePropertyCancel = function(){
+	document.getElementById("collapsible_property").click(); 
+	document.getElementById("property_textarea").value = property_input;
+};
+var handleClassCancel = function(){
+	document.getElementById("collapsible_class").click(); 
+	document.getElementById("class_textarea").value = class_input;
+};
 $("#btn_submit").click(function(){
 	var url = $("#index").val();
 	var data = {};
@@ -49,8 +77,7 @@ $("#btn_submitfile").click(function(){
 		alert("Error occured");
 	});
 });
-
-$(document).ready(function() {
+var getEntityQuery = function(){
 	$.ajax({
 		type : "POST",
 		dataType: "text",
@@ -59,8 +86,11 @@ $(document).ready(function() {
 		async: false,		
 	})
 	.done(function(data){
-		document.getElementById("entity_textarea").value =data;
+		document.getElementById("entity_textarea").value=data;
+		entity_input=data;
 	});
+};
+var getPropertyQuery = function(){
 	$.ajax({
 		type : "POST",
 		dataType: "text",
@@ -69,8 +99,11 @@ $(document).ready(function() {
 		async: false,		
 	})
 	.done(function(data){
-		document.getElementById("property_textarea").value =data;
+		document.getElementById("property_textarea").value=data;
+		property_input = data;
 	});
+};
+var getClassQuery = function(){
 	$.ajax({
 		type : "POST",
 		dataType: "text",
@@ -80,11 +113,21 @@ $(document).ready(function() {
 		async: false,		
 	})
 	.done(function(data){
-		document.getElementById("class_textarea").value =data;
+		document.getElementById("class_textarea").value=data;
+		class_input=data;
 	});
+};
+var addEventListeners = function(){
+	document.getElementById("entity_save").addEventListener("click", changeEntity);
+	document.getElementById("prop_save").addEventListener("click", changeProperty);
+	document.getElementById("class_save").addEventListener("click", changeClass);
+	document.getElementById("entity_cancel").addEventListener("click", handleEntityCancel);
+	document.getElementById("prop_cancel").addEventListener("click", changeProperty);
+	document.getElementById("class_cancel").addEventListener("click", changeClass);
+};
+var toggleButtonHandler = function(){
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
-
 	for (i = 0; i < coll.length; i++) {
 	    coll[i].addEventListener("click", function() {
 	        this.classList.toggle("active");
@@ -96,5 +139,12 @@ $(document).ready(function() {
 	        }
 	    });
 	}
+};
+$(document).ready(function() {
+	getEntityQuery();
+	getPropertyQuery();
+	getClassQuery();
+	toggleButtonHandler();
+	addEventListeners();
 
 });
